a4cb67cd2330ebc268c89b7ef9a30e031b189fd2
monitus2022 <monitus202201@proton.me>
2025-12-04 14:39:05 +0800

    Updated front page intro and site config

--- src/site.config.ts
+++ src/site.config.ts
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "Tommy Cheung - My Website",
+	title: "Tommy Cheung - My Personal Hub",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
 	url: "https://monitus.org",
 };
